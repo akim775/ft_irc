bc6293104a23be9594c5a8f3af93235f851d03a2
Ilan Sadi <ilsadi@f1r9s17.clusters.42paris.fr>
2026-01-07 20:17:58 +0100

    oeoe

--- Client.hpp
+++ Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahamini <ahamini@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ilsadi <ilsadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 12:17:54 by ahamini           #+#    #+#             */
-/*   Updated: 2026/01/05 16:16:14 by ahamini          ###   ########.fr       */
+/*   Updated: 2026/01/07 12:06:59 by ilsadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,42 @@
 #include "Server.hpp"
 
 class Client {
-	private:
-		int	_fd;
-		bool		_hasGivenPassword;
-		bool		_isRegistered;
-		std::string	_nickname;
-		std::string _realname;
-		std::string	_username;
-		std::string	_ip_address;
-		std::string	_buffer;
-		std::vector<std::string>	_joinedChannels;
+private:
+	int	_fd;
+	bool		_hasGivenPassword;
+	bool		_isRegistered;
+	std::string	_nickname;
+	std::string _realname;
+	std::string	_username;
+	std::string	_ip_address;
+	std::string	_buffer;
+	std::vector<std::string>	_joinedChannels;
 
-	public:
-		Client();
-		Client(int fd, struct sockaddr_in addr);
-		
-		// Setters
-		void	setHasGivenPassword(bool value);
-		void	setIsRegistered(bool value);
-		void	setNickname(std::string const &nickname);
-		void	setUsername(std::string const &user);
-		void	setRealname(std::string const &real);
-		void	setFd(int fd);
+public:
+	Client();
+	Client(int fd, struct sockaddr_in addr);
+	
+	// Setters
+	void	setHasGivenPassword(bool value);
+	void	setIsRegistered(bool value);
+	void	setNickname(std::string const &nickname);
+	void	setUsername(std::string const &user);
+	void	setRealname(std::string const &real);
+	void	setFd(int fd);
 
-		// Getters
-		std::string	& getBuffer() { return _buffer; }
-		std::string	getNickname() const;
-		std::string	getIPAdress() const;
-		std::string	getUsername() const;
-		std::string	getRealname() const;
-		bool		hasGivenPassword() const;
-		bool		isRegistered() const;
-		int			getFd() const;
-		void		addChannel(const std::string& channelName);
-		void		removeChannel(const std::string& channelName);
-		size_t		getNbChannels() const;
-		const std::vector<std::string>& getJoinedChannels() const;
+	// Getters
+	std::string	& getBuffer() { return _buffer; }
+	std::string	getNickname() const;
+	std::string	getIPAdress() const;
+	std::string	getUsername() const;
+	std::string	getRealname() const;
+	bool		hasGivenPassword() const;
+	bool		isRegistered() const;
+	int			getFd() const;
+	void		addChannel(const std::string& channelName);
+	void		removeChannel(const std::string& channelName);
+	size_t		getNbChannels() const;
+	const std::vector<std::string>& getJoinedChannels() const;
 };
 
 #endif
